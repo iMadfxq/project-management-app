6d76ece97a63bbfae4d0dd71780693a4336ed213
iMadfxq <imadfxq@gmail.com>
2022-12-01 20:14:09 -0500

    Better way of displaying current projects on the dashboard

--- src/components/ProjectsList/ProjectsList.component.jsx
+++ src/components/ProjectsList/ProjectsList.component.jsx
@@ -2,14 +2,20 @@ import "./ProjectsList.styles.scss";
 
 export default function ProjectsList({ projects }) {
   return (
-    <>
+    <section className="dashboard__projectslist">
       {projects.length === 1 && <p>Sorry, no projects have been created yet</p>}
       {projects.map((p) => (
-        <>
+        <article className="dashboard__projectslist--item" key={p.id}>
           <p>{p.title}</p>
-          <p>{p.details}</p>
-        </>
+          <p>Due: {p.dueDate.toDate().toDateString()}</p>
+          <p>Category: {p.category}</p>
+          <p>Assigned to: {p.assignedToList.map((user) => {
+            return (
+              <span key={user.photoURL}><img src={user.photoURL} alt={user.displayName} title={user.displayName}  /></span>
+            )
+          })}</p>
+        </article>
       ))}
-    </>
+    </section>
   );
 }
