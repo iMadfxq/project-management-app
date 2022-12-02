f34c19ee91e5756ce77fbb519a6400741d6cd8f7
iMadfxq <imadfxq@gmail.com>
2022-12-02 17:44:58 -0500

    Now getting params, using them to fetch individual documents and render them

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,3 +1,28 @@
-export function useDocument() {
+import { useEffect, useState } from "react";
+import { projectFirestore } from "../firebase/config";
 
+export function useDocument(collection, id) {
+  const [document, setDocument] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id)
+
+    const unsub = ref.onSnapshot((snapshot) => {
+      if(snapshot.data()) {
+        setDocument({...snapshot.data(), id: snapshot.id})
+        setError(null)
+
+      } else {
+        setError('Sorry, this documents does not exist')
+      }
+    }, (err) => {
+      console.log(err.message)
+      setError('failed to get the document')
+    })
+
+    return () => unsub()
+  }, [collection, id])
+
+  return {document, error}
 }
--- src/routes/project/project.component.jsx
+++ src/routes/project/project.component.jsx
@@ -1,9 +1,22 @@
+import { useParams } from 'react-router-dom'
+import { useDocument } from '../../hooks/useDocument'
 import './project.styles.scss'
 
 export default function Project( ) {
-  return (
-    <>
-      <p>Project</p>
-    </>
-  )
+  const {id} = useParams()
+  const {document, error} = useDocument('Projects', id)
+
+  if (error) {
+    return <p>{error}</p>
+  }
+  if(!document) {
+    return <p>Loading...</p>
+  }
+  if(document) {
+    return (
+      <>
+        <p>{document.title}</p>
+      </>
+    )
+  }
 }
