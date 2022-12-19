490c1dc2448fa9702a2c1885d880a070aeb50a4d
iMadfxq <imadfxq@gmail.com>
2022-12-18 21:36:17 -0500

    Left side panel now will open on clicks, when switching pages it will close, if clicking outside it will close too

--- src/components/Sidebar/Sidebar.component.jsx
+++ src/components/Sidebar/Sidebar.component.jsx
@@ -9,22 +9,58 @@ import { AuthContext } from '../../context/AuthContext'
 export default function Sidebar({photoURL, displayName}) {
   const {documents } = useCollection('Projects',null,['dueDate'])
   const {user} = useContext(AuthContext)
-  const [open, setOpen] = useState(true)
+  const [openMine, setOpenMine] = useState(true)
+  const [openPanel, setOpenPanel] = useState(false)
+  
 
   const handleClick = () => {
-    setOpen((state) => !state)
-    console.log(open)
+    setOpenMine((state) => !state)
+    console.log(openMine)
   }
   return (
     <>
-    <div className='responsive-openPanel'>
+    <div onClick={() => setOpenPanel(true)} className={!openPanel ?'responsive-openPanel' :'hide'}>
       ➡
       <div>
       <span>🏠</span>
       <p>Panel</p>
       </div>
     </div>
-    <aside>
+    {openPanel && (
+          <>
+          <div className='modal-container' onClick={() => setOpenPanel(false)}>
+            <span>ⓧ</span>
+          </div>
+          <aside className='mobilePanel' >
+          <section>
+            <img src={photoURL} alt="User's thumbnail" />
+            <h3>Hey, {displayName}</h3>
+          </section>
+          <nav >
+            <NavLink onClick={() => {
+            setOpenPanel(false)
+          }} to={'/'}>Dashboard</NavLink>
+            <NavLink onClick={() => {
+            setOpenPanel(false)
+          }} to={'/create'}>Create</NavLink>
+            <h4 title='Most urgent first' onClick={handleClick}>YOUR PROJECTS <span className={!openMine ? 'closed' : ''}>▼</span></h4>
+            {(documents && openMine) && documents.map(doc => {
+                        let assignedToMe = false;
+                        doc.assignedToList.forEach((u) => {
+                          if (u.id === user.uid) {
+                            assignedToMe = true;
+                          }
+                        });
+                        return assignedToMe ? <NavLink onClick={() => {
+                          setOpenPanel(false)
+                        }} key={doc.id} to={`/project/${doc.id}`}>{doc.title}</NavLink> : '';
+            
+            })}
+          </nav>
+        </aside>
+    </>
+    )}
+    <aside className='desktop'>
       <section>
         <img src={photoURL} alt="User's thumbnail" />
         <h3>Hey, {displayName}</h3>
@@ -32,8 +68,8 @@ export default function Sidebar({photoURL, displayName}) {
       <nav>
         <NavLink to={'/'}>Dashboard</NavLink>
         <NavLink to={'/create'}>Create</NavLink>
-        <h4 title='Most urgent first' onClick={handleClick}>YOUR PROJECTS <span className={!open ? 'closed' : ''}>▼</span></h4>
-        {(documents && open) && documents.map(doc => {
+        <h4 title='Most urgent first' onClick={handleClick}>YOUR PROJECTS <span className={!openMine ? 'closed' : ''}>▼</span></h4>
+        {(documents && openMine) && documents.map(doc => {
                     let assignedToMe = false;
                     doc.assignedToList.forEach((u) => {
                       if (u.id === user.uid) {
